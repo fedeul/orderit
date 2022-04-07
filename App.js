b676cfffa90da1a342b7ba25fe11f7473ffa121c
Dashboard Design <fede.ulbrich@gmail.com>
2022-04-06 19:43:59 -0600

    Update de estilos

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, Alert, Pressable } from "react-native";
 import Styles from "./assets/Styles/mainStyles";
 import React, { useState } from "react";
 import ModalItem from "./components/Modal";
 import AddItem from "./components/AddItem";
 import Header from "./components/Header";
+import { useFonts } from "expo-font";
+// import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Mali: require("./assets/fonts/Mali-Regular.ttf"),
+    MaliBold: require("./assets/fonts/Mali-Bold.ttf"),
+  });
+  // if (!loaded) return <AppLoading />;
   const [counter, setCounter] = useState(0);
   const [itemList, setItemList] = useState([]);
   const [itemSelected, setItemSelected] = useState({});
@@ -34,27 +41,28 @@ export default function App() {
     setModalVisible(!modalVisible);
   };
 
+  const deleteAlert = (id) => {
+    Alert.alert("This action can't be undo", "Are you shure?", [
+      {
+        text: "Cancel",
+        onPress: () => closeModal(),
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => onHandlerDelete(id) },
+    ]);
+  };
+
   const renderItem = (data) => (
-    <View style={{ alignContent: "space-around", flexDirection: "row" }}>
-      <Text
-        style={Styles.eachItem}
-        key={data.item.id}
-        value={data.item.value}
-        qty={data.item.qty}
-        onPress={onHandlerModal.bind(this, data.item.id)}
-      >
-        {data.item.value}
-      </Text>
-      <Text
-        style={Styles.eachItemQty}
-        key={data.item.id}
-        value={data.item.value}
-        qty={data.item.qty}
-        onPress={onHandlerModal.bind(this, data.item.id)}
-      >
-        {data.item.qty}
-      </Text>
-    </View>
+    <Pressable
+      onPress={onHandlerModal.bind(this, data.item.id)}
+      value={data.item.value}
+      qty={data.item.qty}
+    >
+      <View style={{ alignContent: "space-around", flexDirection: "row" }}>
+        <Text style={Styles.eachItem}>{data.item.value}</Text>
+        <Text style={Styles.eachItemQty}>{data.item.qty}</Text>
+      </View>
+    </Pressable>
   );
 
   return (
@@ -68,7 +76,7 @@ export default function App() {
           keyExtractor={(item) => item.id}
         />
         <ModalItem
-          onDelete={onHandlerDelete}
+          onDelete={deleteAlert}
           item={itemSelected}
           visible={modalVisible}
           onCancel={closeModal}
--- components/AddItem.jsx
+++ components/AddItem.jsx
@@ -8,10 +8,12 @@ import {
   StyleSheet,
   Pressable,
 } from "react-native";
+import { Dimensions } from "react-native-web";
 
 function AddItem(props) {
   const [textItem, setTextItem] = useState();
   const [numItem, setNumItem] = useState();
+  const [selectedUnit, setSelectedUnit] = useState();
   const { onAddItem } = props;
   const onHandlerChangeItem = (t) => {
     setTextItem(t);
@@ -26,12 +28,17 @@ function AddItem(props) {
     setNumItem("");
     onAddItem(textItem, numItem);
   };
+  const onHandlerCleanInput = () => {
+    setTextItem("");
+    setNumItem("");
+  };
 
   return (
-    <>
+    <View style={StylesThis.containerInputs}>
       <View
         style={{
-          padding: 25,
+          paddingVertical: 25,
+          paddingHorizontal: 5,
           width: "90%",
           backgroundColor: "",
           flexDirection: "row",
@@ -51,29 +58,56 @@ function AddItem(props) {
           onChangeText={onHandlerChangeNumItem}
           keyboardAppearance="dark"
           keyboardType="numeric"
-          maxLength={2}
+          maxLength={3}
         ></TextInput>
       </View>
       {/* Estilo de botón básico */}
       {/* <Button title="+ Add" onPress={onHandlerAddItem} /> */}
-      <Pressable
-        style={[Styles.button, Styles.buttonOK, { width: "80%" }]}
-        onPress={onHandlerAddItem}
-      >
-        <Text style={Styles.textStyle}>+ Add</Text>
-      </Pressable>
-    </>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <Pressable
+          style={[
+            Styles.button,
+            Styles.buttonOK,
+            { width: "70%", marginRight: "5%" },
+          ]}
+          onPress={onHandlerAddItem}
+        >
+          <Text style={Styles.textStyle}>+ Add</Text>
+        </Pressable>
+
+        <Pressable
+          style={[Styles.button, Styles.buttonClear, { width: "15%" }]}
+          onPress={onHandlerCleanInput}
+        >
+          <Text style={{ color: "black", textAlign: "center" }}>🗑</Text>
+        </Pressable>
+      </View>
+    </View>
   );
 }
 
 const StylesThis = StyleSheet.create({
+  containerInputs: {
+    padding: 25,
+    marginTop: 25,
+    borderRadius: 25,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
   textInput: {
     padding: 5,
     borderRadius: 5,
     borderColor: "#dddddd",
     borderBottomWidth: 1,
     marginBottom: 10,
-    width: "70%",
+    width: "75%",
   },
   numInput: {
     padding: 5,
@@ -81,7 +115,7 @@ const StylesThis = StyleSheet.create({
     borderColor: "#dddddd",
     borderBottomWidth: 1,
     marginBottom: 10,
-    width: "30%",
+    width: "25%",
   },
 });
 
