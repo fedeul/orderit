0cd7c0cd492f18d3653c0637ebd9217af87d895f
Dashboard Design <fede.ulbrich@gmail.com>
2022-04-27 18:21:27 -0600

    Añadido de Firebase

--- constants/Database.js
+++ constants/Database.js
@@ -1,1 +1,3 @@
 export const URL_API = "https://orderit-5bbe7-default-rtdb.firebaseio.com/";
+
+export const URL_AUTH_SINGUP = "=AIzaSyDc4wdHaf1cY5O4CVLPxx2_CqCMN7sjXU8";
--- store/index.jsx
+++ store/index.jsx
@@ -4,11 +4,13 @@ import thunk from "redux-thunk";
 import CategoryReducer from "./reducers/category.reducer";
 import ItemReducer from "./reducers/item.reducer";
 import CartReducer from "./reducers/cart.reducer";
+import OrderReducer from "./reducers/order.reducer";
 
 const RootReducer = combineReducers({
   categories: CategoryReducer,
   items: ItemReducer,
   cart: CartReducer,
+  order: OrderReducer,
 });
 
 export default createStore(RootReducer, applyMiddleware(thunk));
